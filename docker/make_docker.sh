06d456a2d0d00b1f30d4a6de1f1cb0c9bb1b35c0
jin-jae <jinjae.dev@gmail.com>
2024-05-24 21:32:49 +0900

    fix: docker sh file

--- docker/make_docker.sh
+++ docker/make_docker.sh
@@ -1,7 +1,18 @@
+display_usage() { 
+	echo "usage: ./make_docker.sh \$PATH_TO_GIT_REPO"
+}
+
 # docker build
 docker build \
-    -t lifelog:latest \
+    -t lifelog \
+    -f docker/Dockerfile \
     .
+
+if [  $# -le 0 ] 
+then 
+    display_usage
+    exit 1
+fi
 
 # docker run
 docker run \
@@ -10,6 +21,6 @@ docker run \
     -p 4242:4242 \
     --ipc=host \
     --name lifelog \
-    -v ..:/workspace \
-    lifelog:latest \
+    -v $1:/workspace \
+    lifelog \
     /bin/bash
